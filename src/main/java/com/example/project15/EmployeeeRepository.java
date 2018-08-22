a070589a650ef773747a23a66d68ee3feb4192bf
gbangsu <gu827@outlook.com>
2018-08-22 16:14:31 -0400

    Initial commit

---
+++ src/main/java/com/example/project15/EmployeeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.project15;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeeRepository extends CrudRepository<Employee, Long> {
+}
---
+++ src/main/java/com/example/project15/Project15Application.java
@@ -0,0 +1,12 @@
+package com.example.project15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project15Application.class, args);
+	}
+}
